af9a38888edcfe52b3c06d8d6ea5cf61e40ecd8c
Shann Wei Yeh <the3dsandwich@the3dsandbook.local>
2019-10-08 11:03:18 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { Text } from "./Text";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const pool = "¡¢£¤¥¦§¨©ª«¬®¯°±²³´µ¶·¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞşŠšŢţŤťŦŧŨũŪūŬŭŮůŰűŲųŴŵŶŷŸŹźŻżŽž  ".split(
+        ""
+    );
+
+    const [text, setText] = useState(null);
+
+    useEffect(() => {
+        const unregister = setInterval(() => {
+            if (Math.random() < 0.1) {
+                let tempText = "";
+                for (let i = 0; i < Math.floor(Math.random() * 5000); i++) {
+                    tempText = tempText.concat(
+                        pool[Math.floor(Math.random() * pool.length)]
+                    );
+                    if (Math.random() < 0.06) tempText = tempText.concat(" ");
+                }
+                setText(tempText);
+            }
+        }, 150);
+        return () => {
+            clearInterval(unregister);
+        };
+    }, [pool, text]);
+
+    return (
+        <div id="app">
+            <Text text={text} />
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Text.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState, useRef } from "react";
+
+function Text({ text }) {
+    const [textState, setTextState] = useState([]);
+    const [displayText, setDisplayText] = useState("");
+
+    useEffect(() => {
+        if (typeof text === "string") {
+            setTextState(text.split(""));
+            setDisplayText("");
+        }
+    }, [text]);
+
+    useInterval(() => {
+        if (textState.length > 0) {
+            let tempText = textState;
+            setDisplayText(displayText.concat(tempText.shift()));
+            setTextState(tempText);
+        }
+    }, 10);
+
+    return (
+        <div id="text">
+            <p>{displayText}</p>
+        </div>
+    );
+}
+
+function useInterval(callback, delay) {
+    const savedCallback = useRef();
+
+    // Remember the latest callback.
+    useEffect(() => {
+        savedCallback.current = callback;
+    }, [callback]);
+
+    // Set up the interval.
+    useEffect(() => {
+        function tick() {
+            savedCallback.current();
+        }
+        if (delay !== null) {
+            let id = setInterval(tick, delay);
+            return () => clearInterval(id);
+        }
+    }, [delay]);
+}
+
+export { Text };
